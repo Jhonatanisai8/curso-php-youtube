4d5915dfedc2731899be3d29a0d2c3c8cc2abd87
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-09 02:18:58 -0500

    varaibles en php

---
+++ 003Variables.php
@@ -0,0 +1,25 @@
+<!--/*En PHP las variables se representan con un signo de dolar-->
+<!--seguido del nombre de la variable*/-->
+<?php
+
+$nombre = "Maria";
+$edad = 12;
+$ciudad = "Lima-Peru";
+$esMayor = false;
+?>
+
+<h1>
+    <?= $nombre; ?>
+</h1>
+<br>
+<h2>
+    <?= $edad; ?>
+</h2>
+
+<style>
+    body {
+        background-color: #444;
+        color: #fff;
+    }
+</style>
+
