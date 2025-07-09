04f022421b4d4519380117759ec7169c6449e8fd
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-09 02:05:13 -0500

    Tipos de Datos

---
+++ 002TiposDatos.php
@@ -0,0 +1,19 @@
+<?php
+echo TRUE;
+echo "<br>";
+echo 4;
+echo "<br>";
+echo "Hola Como estas";
+echo "<br>";
+var_dump("Hola Mundo!");
+echo "<br>";
+echo 234.9;
+?>
+
+<style>
+    body{
+        background-color: #444;
+        color: #fff;
+    }
+</style>
+
