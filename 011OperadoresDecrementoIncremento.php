b5285d524d7fdd0546c3aaf7c977bde223d960bc
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-10 01:10:01 -0500

    imcremento decremto

---
+++ 011OperadoresDecrementoIncremento.php
@@ -0,0 +1,24 @@
+<?php
+echo "<h1>Operadores de Incremento y Decremento.</h1>";
+$numero_1 = 3;
+
+$numero_1++;
+echo "<br>";
+echo $numero_1;
+echo "<br>";
+
+$numero_1--;
+echo "<br>";
+echo $numero_1;
+echo "<br>";
+
+++$numero_1;
+echo "<br>";
+echo $numero_1;
+echo "<br>";
+
+
+--$numero_1;
+echo "<br>";
+echo $numero_1;
+echo "<br>";
