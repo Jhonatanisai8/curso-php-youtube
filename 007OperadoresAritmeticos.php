dd23e248ddc10543ac9f50f0b4650269caf30ca5
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-10 00:28:42 -0500

    operadores aritmeticos

---
+++ 007OperadoresAritmeticos.php
@@ -0,0 +1,25 @@
+<?php
+echo "<h1>Operadores Aritmeticos en PHP</h1>";
+
+$numero_a = 6;
+$numero_b = 4;
+
+echo "Numero A: $numero_a";
+echo "<br>";
+echo "Numero B: $numero_b";
+echo "<br>";
+echo "<br>";
+
+//operaciones
+echo "Suma: " . ($numero_a + $numero_b);
+echo "<br>";
+echo "Resta: " . ($numero_a - $numero_b);
+echo "<br>";
+echo "Division: " . ($numero_a / $numero_b);
+echo "<br>";
+echo "Multiplicacion: " . ($numero_a * $numero_b);
+echo "<br>";
+echo "Exponente: " . ($numero_a ** $numero_b);
+echo "<br>";
+echo "Modulo: " . ($numero_a % $numero_b);
+?>
