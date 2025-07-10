e78a2e881f159881ca8f274b0f0f8fe4c58ca476
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-10 00:19:34 -0500

    concatenacion e interpolacion de variables

---
+++ 006ConcatenacionInterpolacion.php
@@ -0,0 +1,16 @@
+<?php
+$nombre = "Jhonatan Isai";
+$ciudad = "Lima";
+
+//contanemos
+$resultado = $nombre . " " . $ciudad;
+echo "<br>";
+echo $resultado;
+echo "<br>";
+echo "Mi nombres es: " . $nombre;
+
+//interpolacion de variables
+echo "<br>";
+echo "Mi nombres es $nombre y vivo en $ciudad .";
+
+?>
