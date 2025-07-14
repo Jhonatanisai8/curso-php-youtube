1bbc9b12aee9a8b0e7f0b4f92043f4087066f8f9
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-14 00:59:41 -0500

    condicional anidada

---
+++ 021CondicionMultiple.php
@@ -0,0 +1,14 @@
+<?php
+$frutas = "limo";
+switch ($frutas) {
+    case "fresa":
+        echo "La fruta es una fresa";
+        break;
+    case "limon":
+        echo "La fruta es una limon";
+        break;
+    default:
+        echo "La fruta no esta dentro de las opciones";
+        break;
+}
+?>
