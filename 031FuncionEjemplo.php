32bec005668e550d265a51762a8ea076e62163e3
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-17 23:19:29 -0500

    Como INCLUIR y LLAMAR una FUNCION desde otro ARCHIVO PHP

---
+++ 031FuncionEjemplo.php
@@ -0,0 +1,6 @@
+<?php
+
+function saludar($nombre)
+{
+    return "Hola $nombre desde una funcion desde otro archivo PHP";
+}
---
+++ 032IncluirFunciones.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1> Incluir Funciones y llamar una funcion desde otro archivo PHP </h1>";
+
+include("031FuncionEjemplo.php");
+echo "<br>";
+echo "Saludo es : " . saludar("Isai");
