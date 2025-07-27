0b594e15638b5bc81634caccc135278cf4434248
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-27 13:12:11 -0500

    validar variables por metodo get

---
+++ 041ValidarVariableGet.php
@@ -0,0 +1,25 @@
+<?php
+echo  "<h1>VALIDAR VARIABLES EN PHP</h1>";
+// RECUPERAMOS LA VARIABLE MEDIANTE EL METODO GET 
+$numero = $_GET['numero'];
+$nombre = "";
+// para eliminar la variable 
+echo  "<h2>Uso de la funcion 'unset' </h2>";
+// unset($numero);
+if (is_null($numero)) {
+    echo "<p>La variables es nula</p>";
+} else {
+    echo "<p>La variables no es nula</p>";
+    echo "<br>";
+    echo $numero;
+}
+echo "<br>";
+
+echo  "<h2>Uso de la funcion 'empty' </h2>";
+if (empty($nombre)) {
+    echo "<p>La variable esta vacia.</p>";
+} else {
+    echo "<p>La variable no esta vacia.</p>";
+    echo "<br>";
+    echo $nombre;
+}
