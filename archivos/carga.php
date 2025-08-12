aac087caaf49b05a2a559ecb46efe8dc43a10c7b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-11 21:35:51 -0500

    como limitar archivos

--- archivos/carga.php
+++ archivos/carga.php
@@ -4,6 +4,13 @@
 //echo $_FILES['fichero']['type'] . "<br>";
 //echo $_FILES['fichero']['error'] . "<br>";
 //echo $_FILES['fichero']['size'] . "<br>";
+
+if (mime_content_type($_FILES['fichero']['tmp_name']) != "image/jpeg"
+    && mime_content_type($_FILES['fichero']['tmp_name']) != "image/png") {
+    echo "El tipo de fichero no adminitido.";
+    exit();
+}
+
 if (!file_exists("archivos_guardados")) {
     //creamos el directorio en caso no se encuentre
     if (!mkdir("archivos_guardados", 0777)) {
