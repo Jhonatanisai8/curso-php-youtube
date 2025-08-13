9f74ce17c5a5d7bfdcd9d36e280913d1a57a95fe
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-12 22:57:42 -0500

    como eliminar sesiones

--- sessiones/048Cerrar.php
+++ sessiones/048Cerrar.php
@@ -1,1 +1,11 @@
 <?php
+session_name("LOGIN");
+session_start();
+session_destroy();
+/*
+- para eliminar solo las variables
+session_unset();
+
+- para eliminar solo un variable determinada
+unset($_SESSION["Nombre"]);
+ */
