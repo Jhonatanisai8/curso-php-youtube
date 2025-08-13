7bb0d6bbe4f8509bf9ee05e052c4f5743d7cfca5
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-12 23:04:24 -0500

    REDIRECCIONAR paginas usando PHP

--- sessiones/login.php
+++ sessiones/login.php
@@ -1,12 +1,17 @@
 <?php
 if ($_POST['usuario'] == "Jhonatan" &&
     $_POST['password'] == "admin123") {
+
     session_name("LOGIN");
     session_start();
+
     $_SESSION["Nombre"] = "Jhonatan";
     $_SESSION["Apellido"] = "Elias";
     $_SESSION["Pais"] = "Perú";
-    echo "Sesion Iniciada";
+
+    //echo "Sesion Iniciada";
+    //para redireccionar a otra pagina
+    header('Location: 046Sessiones.php');
 } else {
     echo "Datos incorrectos";
 }
